a44c359fac3874c655404bfaf63921267cb34816
Fabrizio Duroni <fabrizio.duroni@gmail.com>
2024-12-31 02:07:02 +0100

    Final tweaks :rocket:

--- parallax-carousel/parallax-carousel-item/parallax-carousel-item.tsx
+++ parallax-carousel/parallax-carousel-item/parallax-carousel-item.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import Animated, {SharedValue} from "react-native-reanimated";
 import {Dimensions, Image, StyleSheet, View} from "react-native";
 import {ParallaxCarouselItemData} from "@/parallax-carousel/parallax-carousel-item/parallax-carousel-item-data";
@@ -6,10 +5,11 @@ import {LinearGradient} from 'expo-linear-gradient';
 import {
     useParallaxWithOpacityAnimations
 } from "@/parallax-carousel/parallax-carousel-item/use-parallax-with-opacity-animations";
+import {FC} from "react";
 
 const { width, height } = Dimensions.get('window');
 
-export const ParallaxCarouselItem: React.FC<{ index: number,   scrollX: SharedValue<number>; item: ParallaxCarouselItemData }> = ({ index, scrollX, item }) => {
+export const ParallaxCarouselItem: FC<{ index: number, scrollX: SharedValue<number>; item: ParallaxCarouselItemData }> = ({ index, scrollX, item }) => {
     const {parallaxAnimatedStyle, opacityAnimatedStyle} = useParallaxWithOpacityAnimations(index, scrollX, width);
 
     return (
